1603625ec1e68e5c40bf163bb87a18dede4622db
pepe1518 <pepe1518@gmail.com>
2014-09-23 17:22:01 -0430

    menu por departamentos

--- application/views/helpers/MenuDepartamentos.php
+++ application/views/helpers/MenuDepartamentos.php
@@ -1,5 +1,5 @@
 <?php
-class Zend_View_Helper_MenuForm extends Zend_View_Helper_Abstract
+class Zend_View_Helper_MenuDepartamentos extends Zend_View_Helper_Abstract
 {
 	public $view;
 	
@@ -8,6 +8,35 @@ class Zend_View_Helper_MenuForm extends Zend_View_Helper_Abstract
 	}
 
 	public function menuDepartamentos() {
+		$menu = "";
+		$departamentoDao = new App_Dao_DepartamentoDao();
+		foreach($departamentoDao->getTodos() as $departamento) {
+			$url = $this->view->url(array('controller'=> 'entidad', 
+										  'action' 	  => 'index',
+										  'id'		  => $departamento->getId()
+									), 'default', true);
+			$nombre = $departamento->getNombre();
+			$menu .= "<li>";
+			$menu .= "<a href=\"". $url ."\">". $nombre ."</a>";
+			$menu .= "</li>";
+		}	
+			
+		$html = "<div class=\"content_left_section\">";
+		$html .= "<h1>Menú</h1>";
+		$html .= "<ul>";	
+		$html .= "<li><h1>Personas</h1></li>";
+		$html .= $menu;
+		$html .= "<li><h1>Instituciones</h1></li>";
+		$html .= $menu;		
+		$html .= "<a href=\"". $this->view->url(
+					array(
+						'controller' => 'user',
+						'action'     => 'logout'
+					), 'default', true) ."\" >Salir</a>";
+		$html .= "</li>";
+		$html .= "</ul>";
+		$html .= "</div>";
+		return $html;
 		
 	}
 }
