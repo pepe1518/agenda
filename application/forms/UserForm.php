9c1c9ccba91c81b48eab3bbe78acb12230211b42
Dyanniza <ceci.chiqui@gmail.com>
2014-09-26 08:33:31 -0400

    mopdificaciones en form layout y hoja de estilos

--- application/forms/UserForm.php
+++ application/forms/UserForm.php
@@ -6,8 +6,36 @@ class Application_Form_UserForm extends Zend_Form
     public function init()
     {
         /* Form Elements & Other Definitions Here ... */
+        $this->setMethod('POST');
+        
+        $usuario = new Zend_Form_Element_Text('_usuario');
+	/*$usuario = $this->createElement('text', '_usuario');*/
+	$usuario->setLabel('Usuario:*');
+	$usuario->setRequired(true);
+        
+        $contrasenia = new Zend_Form_Element_Password('_contrasenia');
+	/*$contrasenia = $this->createElement('text', '_contrasenia');*/
+	$contrasenia->setLabel('Contraseña:*');
+	$contrasenia->setRequired(true);
+        
+        $nombres = new Zend_Form_Element_Text('_nombre');
+	/*$nombres = $this->createElement('text', '_nombre');*/
+	$nombres->setLabel('Nombres:*');
+	$nombres->setRequired(true);
+        
+        $apellidos = new Zend_Form_Element_Text('_apellido');
+	$apellidos->setLabel('Apellidos:*');
+	$apellidos->setRequired(true);
+                
+        	
+        $this->addElements(array( $usuario, $contrasenia, $nombres, $apellidos));
+        
+        
+        
+        
     }
 
 
 }
+
 
